a21a273822604c38e5823b814903ff87bc198a2b
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-04-30 23:13:26 -0300

    Added the zsh syntax highlighting plugin

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ source functions.sh
 if [ -z "$ZSH" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
+# Installs the zsh syntax highlighting plugin
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 # Installs Spaceship
 if [ ! -d "$HOME/.zsh/spaceship" ]; then
     mkdir -p "$HOME/.zsh"
