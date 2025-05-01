440a5947fdd13fcf9bafc529ff13ee979ac990d4
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-05-01 11:52:04 -0300

    Added Ctrl Backspace and Ctrl Delete shortcuts to zsh

---
+++ oh-my-zsh/ctrl_backspace_delete.zsh
@@ -0,0 +1,6 @@
+bindkey -e '^H' backward-kill-word # ctrl backspace
+bindkey -e '^[[3;5~' kill-word # ctrl delete
+
+# Dica: Para saber daonde veio este "^[[3;5~" faça o seguinte:
+# Vá no zsh, pressione ctrl v, seguido pela sequência que você quer
+# Vai aparecer o código dela, aí você pega-a, e cola aqui
