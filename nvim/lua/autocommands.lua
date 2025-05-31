261375623ab95e4b1f44731b0c3edfdf1b9c1ff3
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-05-31 18:14:24 -0300

    Set tabwidth to two spaces in ruby and lua files

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -4,3 +4,12 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.colorcolumn = "50,72"
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "ruby", "eruby", "lua" },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
