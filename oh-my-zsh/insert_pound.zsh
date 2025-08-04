8793031f7b177d8da1408b01a9440c5950bc80cb
Guilherme Natan <guilherme.natan.braga@gmail.com>
2025-08-04 19:24:05 -0300

    Add `Alt #` shortcut to zsh
    This shortcut will comment the current line in a zsh interactive
    section, so it can be used later.
    This commit also fixes the comments in an interactive section appearing
    as pure black, a bug that only occurs in the kitty terminal, due to the
    zsh-syntax-highlighting software.

---
+++ oh-my-zsh/insert_pound.zsh
@@ -0,0 +1,3 @@
+# Alt # Inserts a pound
+setopt interactive_comments
+bindkey -M emacs '\e#' pound-insert
---
+++ oh-my-zsh/kitty_comment_grey.zsh
@@ -0,0 +1,2 @@
+# Fixes comment appearing as pure black for comments in the kitty terminal, for zsh interactive sections
+export ZSH_HIGHLIGHT_STYLES[comment]='fg=244,bold'
